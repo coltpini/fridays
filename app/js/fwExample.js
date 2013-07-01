854c824cfd5d913c7ed2bff5cde4ed141a124289
coltpini <coltpini@gmail.com>
2013-07-01 12:32:24 -0600

    added a box for flickr search terms

--- app/js/fwExample.js
+++ app/js/fwExample.js
@@ -18,19 +18,29 @@ fw('.subber').addListener('click',function(e){
     fw.publish('myCustomEvent');
 });
 
-
-
-fw.jsonp({
-    url: "http://api.flickr.com/services/feeds/photos_public.gne?tags=endine&format=json",
-    callback: "jsoncallback",
-    success: function(json){
-        var imgarr = json.items,
-            i = imgarr.length,
-            cont = fw('body').createChild('article').addClass('flickrCont');
-        while(i--){
-            var item = imgarr[i],
-                img = cont.createChild('img');
-            img.setAttribute('src',item.media.m);
-        }
-    }
+var punchOut = 0;
+fw('#flickrTerm').addListener('keyup', function(){
+    clearTimeout(punchOut);
+    punchOut = setTimeout(getFlickr, 200);
 });
+
+var getFlickr = function(){
+    var cont = fw('[.flickrCont]').length < 1 ? fw('body').createChild('article').addClass('flickrCont') : fw('.flickrCont');
+    fw.jsonp({
+        url: "http://api.flickr.com/services/feeds/photos_public.gne?tags=" + fw('#flickrTerm').value + "&format=json",
+        callback: "jsoncallback",
+        success: function(json){
+            var imgarr = json.items,
+                i = imgarr.length;
+            cont.innerHTML = "";
+            while(i--){
+                var item = imgarr[i],
+                    img = cont.createChild('img');
+                img.setAttribute('src',item.media.m);
+//                var c = cont.createChild('p');
+//                c.innerHTML = item.description;
+            }
+        }
+    });
+};
+getFlickr();
